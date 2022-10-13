3e9e735b5b7f534c53d6f9699e7198cb5d77b6f3
lina-22 <haquefahinur@gmail.com>
2022-10-13 18:52:44 +0200

    Bissmillahir rahmanir rahim Java project

---
+++ src/test/java/com/javaProject/javaBack/JavaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaProject.javaBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
