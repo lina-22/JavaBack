b481514d1ce3352fff065c252626fede76ceacb8
lina-22 <haquefahinur@gmail.com>
2022-10-13 15:13:11 +0200

    Bismillahir rahmanir rahim Spring framework add

---
+++ src/main/java/com/javaProject/javaBack/JavaBackApplication.java
@@ -0,0 +1,13 @@
+package com.javaProject.javaBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBackApplication.class, args);
+	}
+
+}
